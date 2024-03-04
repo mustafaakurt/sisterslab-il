d6a6cf0d3504c5055fa0749009e6f884fc0a8905
Mustafa Kurt <mustafakurt2000@gmail.com>
2024-03-04 15:01:41 +0300

    init project

---
+++ src/main/java/com/mustafaakurt/sisterslabil/SisterslabIlApplication.java
@@ -0,0 +1,13 @@
+package com.mustafaakurt.sisterslabil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisterslabIlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisterslabIlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustafaakurt/sisterslabil/SisterslabIlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafaakurt.sisterslabil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisterslabIlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
